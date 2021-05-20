48321b6b1da448558ce368eab112bf2ba6d91f95
Lee <dltpdnd1234@gmail.com>
2021-05-19 21:21:07 -0500

    TS render & generics

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,12 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
 import {Hasformatter} from './Interfaces/Hasformatter.js'
 
-// let docOne:Hasformatter;
-// let docTwo:Hasformatter;
 
-// docOne = new Invoice ('LEE', 'Webwork', 100);
-// docTwo = new Payment('DAEK', 'Playing', 200);
 
-// let docs: Hasformatter[]=[];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-
-//Interfaces
-// interface IsPerson{
-//   name:string;
-//   age:number;
-//   speak(a:string):void;
-//   spend(a:number):number;
-// }
-// const me: IsPerson = {
-//   name:'LEE',
-//   age:25,
-//   speak(text:string):void{
-//     console.log(text);
-//   },
-//   spend(amount:number):number{
-//     console.log('I spent',amount);
-//     return amount;
-//   }
-// };
-// const greetPerson = (person:IsPerson)=>{
-//   console.log('HI',person.name);
-// }
-// greetPerson(me);
-
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul) 
 
 //DOM & Type Casting
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
@@ -55,18 +26,5 @@ form.addEventListener('submit',(e:Event)=>{
   }else{
     doc = new Payment(toform.value,details.value,amount.valueAsNumber);
   }
-  console.log(doc);
-})
-
-// const invOne = new Invoice('LEE','making a web',100);
-// const invTwo = new Invoice('LEEE','making a web',200);
-// let invoices: Invoice[]= [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// // invTwo.client= 'KIM';
-// console.log(invOne,invTwo);
-
-
-
-
+ list.render(doc,type.value,'end');
+});
---
+++ src/classes/ListTemplate.ts
@@ -0,0 +1,24 @@
+import { Hasformatter } from '../Interfaces/Hasformatter';
+
+export class ListTemplate {
+  constructor(private container:HTMLUListElement){}
+
+
+  render(item:Hasformatter,heading:string, pos:'start' | 'end'){
+    const li = document.createElement('li');
+
+    const h4 = document.createElement('h4');
+    h4.innerText = heading;
+    li.append(h4);
+
+    const p = document.createElement('p');
+    p.innerHTML = item.format();
+    li.append(p);
+
+    if(pos === 'start'){
+      this.container.prepend(li);
+    }else{
+      this.container.append(li);
+    }
+  }
+}
